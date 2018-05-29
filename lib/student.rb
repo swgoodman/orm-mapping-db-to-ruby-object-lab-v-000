9711dd2418fd7dbb31f2a05848c5c793019deeaa
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 02:31:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,9 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student = row[0]
+    new_student.name = row[1]
   end
 
   def self.all
