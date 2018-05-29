12bd078f32b14af67a00937afc0dd8c66d5f3aa0
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 02:52:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,13 +51,19 @@ class Student
       FROM students
       WHERE grade < 12
     SQL
-    
+
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
 
   def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+    
   end
 
   def self.first_student_in_grade_10
