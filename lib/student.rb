cf33f97abb1b9da734f426be5959952249f69607
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 02:52:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,12 +51,13 @@ class Student
       FROM students
       WHERE grade < 12
     SQL
+    
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(number)
   end
 
   def self.first_student_in_grade_10
