bd4405364b7938e40aa2f34037e371b782aed77d
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 02:49:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,12 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9th
+      LIMIT
+    SQL
 
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
