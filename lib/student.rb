a987c9bd520c8a118362bc45477f58569faefaf5
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 02:40:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,14 @@ class Student
   def self.students_below_12th_grade
   end
 
+  def self.first_X_students_in_grade_10
+  end
+
+  def self.first_student_in_grade_10
+  end
+
+  def self.all_students_in_grade_X(grade)
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
